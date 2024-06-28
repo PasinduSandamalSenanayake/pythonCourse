6f45dc15c566fd29f7cbb32535d1dbdb80b708b8
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2024-06-28 10:03:30 +0530

    day-16 finished

--- day16_oop/main.py
+++ day16_oop/main.py
@@ -1,1 +1,40 @@
-print("hello")
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle") # objecctMethod
+# timmy.color("blue")
+# timmy.forward(120)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight) # objectAttribute
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokeman Name",["Pikachu","Squirtle","Charmander"])
+# table.add_column("Type",["Electric","Water","Fire"])
+# table.align= "l"  #table align to left
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"WHat would you like ? ({options}) : ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+

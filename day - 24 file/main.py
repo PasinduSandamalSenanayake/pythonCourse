3948b3955033794ce32f32dee28a7d4f8693ca5e
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2024-08-13 09:07:34 +0530

    day - 24 basic

--- day - 24 file/main.py
+++ day - 24 file/main.py
@@ -1,9 +1,8 @@
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("new/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
 # default mode is "r" - read , "a" mean is read and write, "w" mean is write mode
-a = 5
-with open("my_file.txt", mode="a") as file:
-    file.write(f"\n{a}")
+with open("new/my_file.txt", mode="a") as file:
+    file.write("\nSandamal")
 
